01e1a85489f6a370853e64e798626478b2346ce0
Brett Earle <brettearle@gmail.com>
2024-03-22 17:19:27 +1030

    hook up router

--- cmd/tishet/main.go
+++ cmd/tishet/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	http.ListenAndServe(":8080", Router())
 }
+
+func Router() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HomeHandler)
+	return mux
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	indexTmpl, err := template.New("index").Parse(indexHTML)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = indexTmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
+
+const indexHTML = `
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<h1>Welcome to Tishet</h1>
+</body>
+</html>
+`
--- cmd/tishet/main_test.go
+++ cmd/tishet/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -8,6 +10,41 @@ func TestHelloWorld(t *testing.T) {
 	t.Log("Hello, World!")
 	t.Run("Test Hello World", func(t *testing.T) {
 		t.Log("Hello, from t run")
+	})
+}
+
+func TestHomeHandler(t *testing.T) {
+	t.Run(" '/' route returns status 200", func(t *testing.T) {
+		request := httptest.NewRequest("GET", "/", nil)
+		response := httptest.NewRecorder()
+		HomeHandler(response, request)
+		if response.Result().StatusCode != 200 {
+			t.Errorf("Expected Status %v but got %v", 200, response.Result().StatusCode)
+		}
+	})
+	t.Run(" '/' route returns html content type", func(t *testing.T) {
+		request := httptest.NewRequest("GET", "/", nil)
+		response := httptest.NewRecorder()
+		HomeHandler(response, request)
+		contentHeader := response.Header().Get("Content-Type")
+		if contentHeader != "text/html" {
+			t.Errorf("Expected Content-Type %v but got %v", "text/html", contentHeader)
+		}
+	})
+
+	t.Run(" '/' returns a html body", func(t *testing.T) {
+		request := httptest.NewRequest("GET", "/", nil)
+		response := httptest.NewRecorder()
+		HomeHandler(response, request)
+		body := response.Body.String()
+		//test if the body is a valid html document without checking the content
+		if !strings.Contains(body, "<html>") || !strings.Contains(body, "</html>") {
+			t.Errorf("Expected Body to be a valid html document")
+		}
+		//contains a title
+		if !strings.Contains(body, "<title>") || !strings.Contains(body, "</title>") {
+			t.Errorf("Expected Body to contain a title")
+		}
 	})
 }
 
